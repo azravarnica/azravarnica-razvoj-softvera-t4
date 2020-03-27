7d30b0934d5516565049996ab45ccfe896ca8f50
azravarnica <avarnica1@etf.unsa.ba>
2020-03-27 21:13:08 +0100

    throw ispisi

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -25,14 +25,23 @@ public class Artikal {
     }
 
     public void setSifra(String sifra) {
+        if(sifra.isEmpty()){
+            throw new IllegalArgumentException("Šifra je prazna");
+        }
         this.sifra = sifra;
     }
 
     public void setNaziv(String naziv) {
+        if(naziv.isEmpty()){
+            throw new IllegalArgumentException("Naziv je prazna");
+        }
         this.naziv = naziv;
     }
 
     public void setCijena(double cijena) {
+        if(cijena<0){
+            throw new IllegalArgumentException("Cijena je negativna");
+        }
         this.cijena = cijena;
     }
 
